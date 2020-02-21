537ae98ed4305dff1f90f5384a2d43600bf5da04
Zeerak N. Wyne <zeerak.wyne@gmail.com>
2020-02-21 18:39:53 +0500

    new dashboard designing

---
+++ new_dash.py
@@ -0,0 +1,11 @@
+# Import required libraries
+import pickle
+import copy
+import pathlib
+import dash
+import math
+import datetime as dt
+import pandas as pd
+from dash.dependencies import Input, Output, State, ClientsideFunction
+import dash_core_components as dcc
+import dash_html_components as html
